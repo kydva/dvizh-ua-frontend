89ff4ee0c110d4196b5064de06dbeda3097df6f9
Vladyslav Y <kydvayv@gmail.com>
2021-11-06 20:31:09 +0200

    Set up eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,26 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+
+  extends: ['@nuxtjs/eslint-config-typescript', 'airbnb-base', 'prettier'],
+
+  plugins: [],
+
+  rules: {
+    'linebreak-style': 'off',
+    'no-shadow': 'off',
+    'no-unused-vars': 'off',
+    'vue/padding-line-between-blocks': ['error', 'always'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: '*' },
+    ],
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+
+    'no-param-reassign': 'off',
+  },
+};
